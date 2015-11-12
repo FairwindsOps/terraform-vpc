a836f01dad8dcaaa5451f6145fa5cbe579555c84
Sarah Zelechoski <sarah@reactiveops.com>
2015-11-12 17:33:50 -0500

    separate nat security rules from security group

--- security-groups.tf
+++ security-groups.tf
@@ -3,82 +3,88 @@ resource "aws_security_group" "nat" {
 	name = "admin_nat"
 	description = "Allow services from the private subnet through NAT"
 	vpc_id = "${aws_vpc.default.id}"
-
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 80
-		to_port = 80
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 123
-		to_port = 123
-		protocol = "udp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 443
-		to_port = 443
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 4443
-		to_port = 4443
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	# git
-	ingress {
-		from_port = 9418
-		to_port = 9418
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	# APNS
-	ingress {
-		from_port = 5223
-		to_port = 5223
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	# APNS
-	ingress {
-		from_port = 2195
-		to_port = 2196
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		cidr_blocks = ["0.0.0.0/0"]
-		from_port = -1
-		to_port = -1
-		protocol = "icmp"
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
 	tags {
 		Name = "admin_nat"
 	}
+}
 
+resource "aws_security_group_rule" "nat_allow_http_to_world" {
+  type = "egress" 
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_https_to_world" {
+  type = "egress" 
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_ntp_to_world" {
+  type = "egress" 
+  from_port = 123
+  to_port = 123
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_git_to_github" {
+  type = "egress" 
+  from_port = 9418
+  to_port = 9418
+  protocol = "tcp"
+  cidr_blocks = ["192.30.252.0/22"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_ssh_to_github" {
+  type = "egress" 
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["192.30.252.0/22"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_ping_to_world" {
+  type = "egress" 
+  from_port = -1
+  to_port = -1
+  protocol = "icmp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_ping_from_world" {
+  type = "ingress" 
+  from_port = -1
+  to_port = -1
+  protocol = "icmp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_all_tcp_traffic_from_vpc" {
+  type = "ingress" 
+  from_port = 1
+  to_port = 65535
+  protocol = "tcp"
+  cidr_blocks = ["${aws_vpc.default.cidr_block}"]
+  security_group_id = "${aws_security_group.nat.id}"
+}
+
+resource "aws_security_group_rule" "nat_allow_all_udp_traffic_from_vpc" {
+  type = "ingress" 
+  from_port = 1
+  to_port = 65535
+  protocol = "udp"
+  cidr_blocks = ["${aws_vpc.default.cidr_block}"]
+  security_group_id = "${aws_security_group.nat.id}"
 }
