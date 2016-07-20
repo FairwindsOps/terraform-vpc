487aca5b6bc323be1ffd0003285d5d14e3137148
Sarah Zelechoski <sarah.zele@gmail.com>
2016-07-20 09:48:23 -0400

    two spaces per indent

--- route-table.tf
+++ route-table.tf
@@ -1,12 +1,12 @@
 # Routing table for public subnets
 resource "aws_route_table" "public" {
-	vpc_id = "${aws_vpc.default.id}"
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.default.id}"
-	}
+  vpc_id = "${aws_vpc.default.id}"
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.default.id}"
+    }
   tags {
-      Name = "public"
+    Name = "public"
   }
 }
 
@@ -15,12 +15,12 @@ resource "aws_route_table" "public" {
 # Gateways were enabled via variable flags and created via TF.
 resource "aws_route_table" "private" {
   count = "${var.az_count}"
-	vpc_id = "${aws_vpc.default.id}"
-	route {
-		cidr_block = "0.0.0.0/0"
-		instance_id = "${element(aws_instance.nat.*.id, count.index)}"
-		nat_gateway_id = "${element(aws_nat_gateway.nat_gateway.*.id, count.index)}"
-	}
+  vpc_id = "${aws_vpc.default.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${element(aws_instance.nat.*.id, count.index)}"
+    nat_gateway_id = "${element(aws_nat_gateway.nat_gateway.*.id, count.index)}"
+  }
   tags {
       Name = "private_az${(count.index +1)}"
   }
