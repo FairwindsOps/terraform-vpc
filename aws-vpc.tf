484239e56e3608ae297120d92b99805e01a3f1a4
Sarah Zelechoski <sarah.zele@gmail.com>
2016-07-20 09:53:23 -0400

    two spaces per indent

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-	access_key = "${var.aws_access_key}"
-	secret_key = "${var.aws_secret_key}"
-	region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
 }
