518a927d6782b7cdf420288286ad45655461d843
Dave Konopka <dave.konopka@gmail.com>
2016-08-26 08:24:51 -0400

    split out route table route creation
    Terraform's `element` function was updated to throw exceptions when provided an empty list. Previously it just returned `nil` and the output was ignored for the nat instance/gateway scenarios.
    
    This splits the route table routes out into separate resources. This avoids the `element` evaluation issue.

--- route-table.tf
+++ route-table.tf
@@ -11,17 +11,26 @@ resource "aws_route_table" "public" {
 }
 
 # Routing table for private subnets
-# Populates `instance_id` if NAT instances or `nat_gateway_id` if NAT
-# Gateways were enabled via variable flags and created via TF.
 resource "aws_route_table" "private" {
   count = "${var.az_count}"
   vpc_id = "${aws_vpc.default.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    instance_id = "${element(aws_instance.nat.*.id, count.index)}"
-    nat_gateway_id = "${element(aws_nat_gateway.nat_gateway.*.id, count.index)}"
-  }
   tags {
       Name = "private_az${(count.index +1)}"
   }
+}
+
+resource "aws_route" "private_nat_instance" {
+  count = "${var.az_count * var.nat_instance_enabled}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  instance_id = "${element(aws_instance.nat.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  depends_on = ["aws_route_table.private","aws_instance.nat"]
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = "${var.az_count * var.nat_gateway_enabled}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  nat_gateway_id = "${element(aws_nat_gateway.nat_gateway.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  depends_on = ["aws_route_table.private","aws_nat_gateway.nat_gateway"]
 }
