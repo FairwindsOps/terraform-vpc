33b51a12c30f861e4533a60ea4d1306c5e6308aa
EJ Etherington <ej@reactiveops.com>
2018-04-26 07:58:34 -0700

    Recent changes in kops have it putting shared tags on the vpc and the nat gateways. Its difficult to add those to the terraform because the tag key is the name of the cluster. Instead of changing those on each run, I'm setting it to ignore changes on tags after creation and letting kops do its thing

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -17,6 +17,9 @@ resource "aws_nat_gateway" "nat_gateway" {
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   allocation_id = "${element(aws_eip.mod_nat.*.id, count.index)}"
   depends_on = ["aws_internet_gateway.default","aws_eip.mod_nat","aws_subnet.public"]
+  lifecycle = {
+                ignore_changes = ["tags"]
+              }
 }
 
 output "aws_nat_gateway_count" {
