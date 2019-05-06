e342cc18227de0b81d26baa23587ad1dc74a3b8c
Micah Huber <micah.huber@mailbox.org>
2019-05-06 13:29:15 -0400

    Add global_tags to elastic ips & nat gateways.

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -16,6 +16,7 @@ resource "aws_nat_gateway" "nat_gateway" {
   count = "${((var.multi_az_nat_gateway * var.az_count) + (var.single_nat_gateway * 1))}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   allocation_id = "${element(aws_eip.mod_nat.*.id, count.index)}"
+  tags = "${var.global_tags}"
   depends_on = ["aws_internet_gateway.default","aws_eip.mod_nat","aws_subnet.public"]
   lifecycle = {
                 ignore_changes = ["tags"]
