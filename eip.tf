5fc5894d00b198232f1ed1933105b364b2a307ad
Adam Zahorscak <69020096+Azahorscak@users.noreply.github.com>
2022-04-06 16:18:49 -0400

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -14,7 +14,10 @@
 
 resource "aws_eip" "mod_nat" {
   count = var.multi_az_nat_gateway * var.az_count + var.single_nat_gateway * 1
-  tags  = var.global_tags
+  tags = merge(
+    var.global_tags,
+    var.elastic_ip_tags,
+  )
   vpc   = true
 }
 
